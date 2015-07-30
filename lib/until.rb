0f72b8d38e6868a400bdd776a7056c04013eb783
GabiOC <oconnor.gabriele@gmail.com>
2015-07-30 13:26:15 -0400

    Add while and until loops

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,11 @@
 def using_until
     levitation_force = 6
     #your code here
-    
+    phrase = "Wingardium Leviosa"
+
+    while levitation_force < 10
+    	puts phrase
+    	levitation_force += 1
+    end
 end
 
--- lib/while.rb
+++ lib/while.rb
@@ -2,6 +2,12 @@ def using_while
 	levitation_force = 6
 	
 	#your code here
+	phrase = "Wingardium Leviosa"
+
+	until levitation_force == 10
+		puts phrase
+		levitation_force += 1
+	end
 end
 
 
